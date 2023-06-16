a80a0c73015682c5d0a5319025e28065187fe01b
olaguru <dapoodedeyi03@gmail.com>
2023-06-16 15:16:56 -0700

    alphabets lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print alpha in caps and low case
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+}
