e818e4d7abc9b5f691e95ec69798db5c123eb5c3
olaguru <dapoodedeyi03@gmail.com>
2023-06-16 22:27:23 -0700

    last task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,44 +7,36 @@
  */
 int main(void)
 {
-	int num1, num2, num3, num4;
+	int i, j;
+	int a, b, c, d;
 
-	num1, num2, num3, num4 = 0;
-
-	while (num1 <= '9')
+	for (i = 0; i < 100; i++)
 	{
-		while (num2 <= '9')
+		a = i / 10;
+		b = i % 10;
+
+		for (j = 0; j < 100; j++)
 		{
-			while (num3 <= '9')
+			c = j / 10;
+			d = j % 10;
+
+			if (a < c || (a == c && b < d))
 			{
-				while (num4 <= '9')
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
 				{
-					if (((num3 + num4) > (num1 + num2) && num3 >= num1) || num1 < num3)
-					{
-						putchar(num1);
-						putchar(num2);
-						putchar(' ');
-						putchar(num3);
-						putchar(num4);
-
-				if (num1 + num2 + num3 + num4 == '179' && num1 == '9')
-					{
-						break;
-					}
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					}
+					putchar(44);
+					putchar(32);
 				}
-				num3++;
 			}
-
-			num2++;
 		}
-		num1++;
 	}
-	putchar('\n');
+	putchar(10);
+
 	return (0);
 }
