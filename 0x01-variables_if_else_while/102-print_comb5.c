eaf10256ba6e75ed6fa2e8e26afb62456846fffd
olaguru <dapoodedeyi03@gmail.com>
2023-06-16 21:58:40 -0700

    last

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/**
+ * main - prints 4 digits separated by space and comma
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	int num1, num2, num3, num4;
+
+	num1, num2, num3, num4 = 0;
+
+	while (num1 <= '9')
+	{
+		while (num2 <= '9')
+		{
+			while (num3 <= '9')
+			{
+				while (num4 <= '9')
+				{
+					if (((num3 + num4) > (num1 + num2) && num3 >= num1) || num1 < num3)
+					{
+						putchar(num1);
+						putchar(num2);
+						putchar(' ');
+						putchar(num3);
+						putchar(num4);
+
+				if (num1 + num2 + num3 + num4 == '179' && num1 == '9')
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+				num3++;
+			}
+
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
