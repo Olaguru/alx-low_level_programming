f4d0cc4be528a4d1e72be92587bc45bfd0efc49b
olaguru <dapoodedeyi03@gmail.com>
2023-06-20 15:00:00 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - print _putchar
  *
@@ -12,9 +12,9 @@ int main(void)
 
 	for (i = 0; i <= 7; i++)
 	{
-		putchar(c[i]);
+		_putchar(c[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 
 	return (0);
 }
