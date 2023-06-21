e0d792ea2aa6b180aaa1d8560e8793ebd99310fa
olaguru <dapoodedeyi03@gmail.com>
2023-06-21 06:12:13 -0700

    fibonacci sum of even of 4million or less

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - sum up even fibonacci up to 4million
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	long int i, x = 1, y = 2, sum = 0, tsum = 0;
+
+	for (i = 0; i < 49; i++)
+	{
+		if ((y % 2 == 0) && (y <= 4000000))
+		{
+			tsum = tsum + y;
+		}
+		sum = x + y;
+		x = y;
+		y = sum;
+	}
+
+	return (0);
+}
