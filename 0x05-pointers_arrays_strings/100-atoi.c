d594102de7352313481d79dbd3d13c69b4f1644a
olaguru <dapoodedeyi03@gmail.com>
2023-06-29 03:47:11 -0700

    a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,37 +1,35 @@
 #include "main.h"
+
 /**
- * _atoi - converts string to an int
- *
- * @s: input string
- *
- * Return: converted string to int
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
  */
 int _atoi(char *s)
 {
-	int sign = 1;	/* x by -1 to get -ve and 1 for +ve */
-	int result = 0;	/* help get the inremented result */
-	int i = 0;	/* for the loop */
-	/* get the sign before the number*/
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	if (s[0] == '-')
+	while (*(s + count) != '\0')
 	{
-		sign = -1;
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
 	}
 
-	/* convert string to an int */
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			if (i == 0)
-			{
-				if (s[i] >= '0' && s[i] <= '9')
-				result = s[i] - '0';
-			}
-			else if (i >= 1)
-		{
-			if (s[i] >= '0' && s[i] <= '9')
-			result = result * 10 + (s[i] - '0');
-		}
-		}
-
-	return (result * sign);
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
