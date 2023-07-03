86f31675c2f4b60a947f6a4ffdd98b52d22bf5f5
olaguru <dapoodedeyi03@gmail.com>
2023-07-03 10:49:12 -0700

    a function that adds two numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ char *cap_string(char *str);
 char *leet(char *str);
 char *rot13(char *str);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void rev_string(char *n);
 
 #endif
