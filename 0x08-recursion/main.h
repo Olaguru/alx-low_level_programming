1af4fc5dbf8e2f707f00efd06f8fa4bd0ad02a77
olaguru <dapoodedeyi03@gmail.com>
2023-07-05 15:43:49 -0700

    a function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,8 @@ int _sqrt_recursion(int n);
 int sqrt_a(int a, int b);
 int is_prime_number(int n);
 int prime_a(int a, int b);
+int is_palindrome(char *s);
+int compare_string(char *s, int left, int right);
+int _strlen_recursion(char *s);
 
 #endif
