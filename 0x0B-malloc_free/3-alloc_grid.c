84467ae290ea505ac6cf4bb44ef4fd12e154d1a4
olaguru <dapoodedeyi03@gmail.com>
2023-07-13 12:58:05 -0700

    allocate memory for pointer of an array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - return a pointer to a 2 dimention array
+ *
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: pointer to an array of int..
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **gridout;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
+	{
+		free(gridout);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+			free(gridout);
+			free(gridout[i]);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
+
+	return (gridout);
+}
