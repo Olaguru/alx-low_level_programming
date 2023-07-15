1982c1e38e3505a4cc4d3c44ef92028ef4fdb132
olaguru <dapoodedeyi03@gmail.com>
2023-07-15 02:08:20 -0700

    test

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 
+#define MAX_REASONABLE_SIZE 1000000000
 /**
  * malloc_checked - Main Entry
  * @b: input
@@ -10,6 +11,11 @@ void *malloc_checked(unsigned int b)
 {
 	void *p;
 
+	if (b > MAX_REASONABLE_SIZE)
+	{
+	return (NULL);
+	}
+	
 	p = malloc(b);
 	if (p == NULL)
 		exit(98);
