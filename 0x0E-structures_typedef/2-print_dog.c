37bc29eb0fc799cff29b3b4eeb08241462a959fa
olaguru <dapoodedeyi03@gmail.com>
2023-07-18 18:04:09 -0700

    new name for the struct dog using typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - print the struct declared for dog d
+ *
+ * @d: the new dog d of type struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!((*d).name))
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+
+		if (!(d->owner))
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", (*d).owner);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,6 +10,12 @@
  * Description: first struct with alx
  */
 
+/**
+ * dog_t - new name for struct dog
+ */
+
+typedef struct dog dog_t;
+
 struct dog
 {
 	char *name;
@@ -18,4 +24,5 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
