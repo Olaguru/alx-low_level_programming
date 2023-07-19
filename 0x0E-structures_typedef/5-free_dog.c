5cb2b85c929762f00aecc4df9b0cfaa7e811d296
olaguru <dapoodedeyi03@gmail.com>
2023-07-18 18:42:59 -0700

    free space

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - a function to free memory of dog
+ * created
+ *
+ * @d: pointer to dog_t
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
