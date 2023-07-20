4ce72dfb4242b217665cb6533ec03af94a0871ef
olaguru <dapoodedeyi03@gmail.com>
2023-07-20 07:49:35 -0700

    check for int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - search for an integer
+ * @array: target array holding int
+ * @size: array size
+ * @cmp: function pointer to compare search int
+ *
+ * Return: int elemnt match, -1 if size <=0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (1);
+	}
+	return (-1);
+}
