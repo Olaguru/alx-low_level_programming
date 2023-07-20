6a0a4134de955b69fba3fee25364a888747eeab2
olaguru <dapoodedeyi03@gmail.com>
2023-07-20 07:51:41 -0700

    print int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,10 +14,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || cmp == NULL)
 		return (-1);
 
-	for (i = 0; i < size i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return (1);
+			return (i);
 	}
 	return (-1);
 }
