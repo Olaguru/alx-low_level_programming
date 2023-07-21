b4544bc70f607d5ded4b74a0b4c3e28b49b66a23
olaguru <dapoodedeyi03@gmail.com>
2023-07-21 10:35:07 -0700

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,20 +1,17 @@
 #include "variadic_functions.h"
 /**
- * print_all - print any argument provided to
- * function
- * @format: the argument specifier
+ * print_all - print any argument provided
+ * @format: argument specifier
  *
- * Return: any argument given based on format specifier
+ * Return: any argument given based on specified format
  */
 void print_all(const char * const format, ...)
 {
-	int i, check_stat;
-
+	int i, check_stat; /* declare variables and va_arg datatype */
 	char *str;
 	va_list spc;
 
-	va_start(spc, format);
-
+	va_start(spc, format); /* initilaize var arguments */
 	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
@@ -22,7 +19,7 @@ void print_all(const char * const format, ...)
 		{
 			case 'i':
 				printf("%d", va_arg(spc, int));
-				check_stat = 0;
+				check_stat = 0; /* check if condition has been met */
 				break;
 			case 'f':
 				printf("%f", va_arg(spc, double));
@@ -37,15 +34,16 @@ void print_all(const char * const format, ...)
 				if (str == NULL)
 					str = "(nil)";
 				printf("%s", str);
+				check_stat = 0;
 				break;
 			default:
 				check_stat = 1;
 				break;
 		}
-		if (format[i + 1] != '\0' && check_stat == 0)
+		if (format[i +  1] != '\0' && check_stat == 0) /* if NOT NULL */
 			printf(", ");
-		i++;
+		i++; /* update step of iter var */
 	}
 	printf("\n");
-	va_end(spc);
+	va_end(spc); /* end traversal */
 }
