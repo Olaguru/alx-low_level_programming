5228ac59db1a49a79899fa3bf834cc77538330d2
olaguru <dapoodedeyi03@gmail.com>
2023-07-20 23:11:49 -0700

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,7 +5,7 @@
  * main - perform the calculation
  * @argc: argument count
  * @argv: argument vector
- * return: result
+ * Return: result
  */
 int main(int argc, char *argv[])
 {
@@ -21,6 +21,12 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	op = (argv[2]);
 	num2 = atoi(argv[3]);
+
+	if (get_op_fun(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
