90dbafa3d576e3442bcc33035657868cec4a09ed
olaguru <dapoodedeyi03@gmail.com>
2023-07-27 18:40:34 -0700

    print the content of a singly lists

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string (malloced string)
+ * @len: length of the string
+ * @next: pointer to the next node
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+#endif
