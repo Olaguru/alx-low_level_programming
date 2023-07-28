b4d5fc1506ef4d64076079a2524a334f171c1fa1
olaguru <dapoodedeyi03@gmail.com>
2023-07-27 20:00:58 -0700

    make my function come first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void ComeFirstbyCTC(void)__attribute__ ((constructor));
+/**
+ * ComeFirst - Function that runs before the main function
+ */
+void ComeFirstbyCTC(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
