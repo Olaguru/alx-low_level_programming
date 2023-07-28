d21f6226afe53e3f28dbc7dd17fa5817d7845e37
olaguru <dapoodedeyi03@gmail.com>
2023-07-27 19:52:06 -0700

    free nodes

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_list - frees a list of nodes
+ * @head: pointer to the first node
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+	free(head->str);
+	free(head);
+}
