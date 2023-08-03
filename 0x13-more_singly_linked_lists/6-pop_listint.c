d3b869a8d47f24aee4ed5e3f8aec38deeecf242f
olaguru <dapoodedeyi03@gmail.com>
2023-08-02 17:53:45 -0700

    delete first node of a list and return its data

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - delete the first node of a list and return
+ * its data
+ * @head: the pointer to the list
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *tmp;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	n = (*head)->n;
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
+	return (n);
+}
