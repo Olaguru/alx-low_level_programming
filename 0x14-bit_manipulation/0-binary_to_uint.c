77fb2a4f481afa04645d3071596b988114705620
olaguru <dapoodedeyi03@gmail.com>
2023-08-05 16:20:12 -0700

    convert binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,7 +22,9 @@ unsigned int binary_to_uint(const char *b)
 	for (len = 0; b[len] != '\0'; len++)
 		;
 	if (len == 1 && (b[0] == '0' || b[0] == '1'))
-			return (b[0] - 48);
+	{
+		return (b[0] - 48);
+	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
