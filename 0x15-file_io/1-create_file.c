38dc3fca5c9cc6c534405d45d45aee094a5added
olaguru <dapoodedeyi03@gmail.com>
2023-08-08 16:11:36 -0700

    create a file and write into it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - create and write into a file
+ * and copy content in it
+ * @filename: the filename to be created
+ * @text_content: the content to be copied into the file
+ * Return: 1 if suceesful or -1 if failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+	}
