3c84db3d46606086b992f716f743693e21b242a1
olaguru <dapoodedeyi03@gmail.com>
2023-08-08 13:17:31 -0700

    read a text from a file and prints the letters read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file and prints
+ * it to the POSIX standard output
+ *
+ * @filename: filename (string pointer)
+ * @letters: number of letters printed
+ * Return: number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buff;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	nrd = read(fd, buff, letters);
+	nwr = write(STDOUT_FILENO, buff, nrd);
+
+	close(fd);
+	free(buff);
+
+	return (nwr);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+0x15-file_io
+The file IO task and file descriptor
